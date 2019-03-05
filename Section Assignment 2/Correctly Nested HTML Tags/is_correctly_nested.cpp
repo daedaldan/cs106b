18e2c60e3260898341a92b28d77f5cafa58739d4
Dan Wang <daniel22373@gmail.com>
2019-03-05 17:26:23 -0500

    Finished 2.3

--- Section Assignment 2/Correctly Nested HTML Tags/is_correctly_nested.cpp
+++ Section Assignment 2/Correctly Nested HTML Tags/is_correctly_nested.cpp
@@ -1,35 +1,62 @@
 // CS106B: Programming Abstractions Assignment 2 Problem 3
 
 #include <iostream>
+#include <vector>
 #include <stack>
+#include <queue>
 
 using namespace std;
 
 bool isCorrectlyNested (string htmlStr) {
-    stack<string> tags;
-    cout << htmlStr << endl;
-    cout << htmlStr.length() << endl;
+    // read the string and put all opening and closing tabs in a vector
+    vector<string> tags;
     for (int i = 0; i < htmlStr.length(); i++) {
         if (htmlStr[i] == '<') {
             string tag;
-            while (htmlStr[i-1] != '>') {
+            while (htmlStr[i] != '>') {
                 tag += htmlStr[i];
                 i++;
             }
-            cout << tag << endl;
+            tag += ">";
+            tags.push_back(tag);
         }
     }
-    // read through file
-        // put opening tags in stack
-    // read through file
-        // if closing tag matches opening tag on top of stack, pop from stack
-        // else, return false
+    
+    // put opening tags in stack
+    stack<string> openingTags;
+    for (int i = 0; i < tags.size(); i++) {
+        if (tags[i][1] != '/') openingTags.push(tags[i]);
+    }
+    
+    // put closing tags in queue
+    queue<string> closingTags;
+    for (int i = 0; i < tags.size(); i++) {
+        if (tags[i][1] == '/') closingTags.push(tags[i]);
+    }
+    
+    // return false if opening and closing tags don't match
+    while (openingTags.size() != 0) {
+        if (openingTags.top() != ('<' + closingTags.front().substr(2))) return false;
+        openingTags.pop();
+        closingTags.pop();
+    }
+    
     return true;
 }
 
 int main() {
     string test1 = "<html><b><i>CS106 rules!</i></b></html>";
     string test2 = "<html><bleh><b><i>CS106 rulessss?</i></b></html>";
-    isCorrectlyNested(test1);
+    
+    if (isCorrectlyNested(test1))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+    
+    if (isCorrectlyNested(test2))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+    
     return 0;
 }
