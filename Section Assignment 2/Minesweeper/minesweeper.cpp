14e89df4ba5a60ead2a4fcc0b2fafa34f7a3b496
Dan Wang <daniel22373@gmail.com>
2019-03-09 11:08:50 -0500

    Finished 2.5

--- Section Assignment 2/Minesweeper/minesweeper.cpp
+++ Section Assignment 2/Minesweeper/minesweeper.cpp
@@ -14,33 +14,57 @@ vector<vector<int>> makeGridOfCounts(vector<vector<bool>> & bombLocations) {
             int bombs = 0;
 
             /* if on top left */ if (i == 0 && j == 0) {
-                for (int x = i; x <= i + 1; x++) {
-                    for (int y = j; y <= j + 1; y++) {
-                        if (bombLocations[x][y]) bombs += 1;
+                for (int y = i; y <= i + 1; y++) {
+                    for (int x = j; x <= j + 1; x++) {
+                        if (bombLocations[y][x]) bombs += 1;
                     }
                 }
             } /* if on top right */ else if (i == 0 && (j == counts[i].size() - 1)) {
-                for (int x = i - 1; x <= i; x++) {
-                    for (int y = j; y <= j + 1; y++) {
-                        if (bombLocations[x][y]) bombs += 1;
+                for (int y = i; y <= i + 1; y++) {
+                    for (int x = j - 1; x <= j; x++) {
+                        if (bombLocations[y][x]) bombs += 1;
                     }
                 }
             } /* if on bottom left */ else if ((i == counts.size() - 1) && j == 0) {
-                for (int x = i - 1; x <= i; x++) {
-                    for (int y = j; y <= j + 1; y++) {
-                        if (bombLocations[x][y]) bombs += 1;
+                for (int y = i - 1; y <= i; y++) {
+                    for (int x = j; x <= j + 1; x++) {
+                        if (bombLocations[y][x]) bombs += 1;
                     }
                 }
             } /* if on bottom right */ else if ((i == counts.size() - 1) && (j == counts.size() - 1)) {
-                for (int x = i - 1; x <= i; x++) {
-                    for (int y = j - 1; y <= j; y++) {
-                        if (bombLocations[x][y]) bombs += 1;
+                for (int y = i - 1; y <= i; y++) {
+                    for (int x = j - 1; x <= j; x++) {
+                        if (bombLocations[y][x]) bombs += 1;
+                    }
+                }
+            } /* if on top */ else if (i == 0) {
+                for (int y = i; y <= i + 1; y++) {
+                    for (int x = j - 1; x <= j + 1; x++) {
+                        if (bombLocations[y][x]) bombs += 1;
+                    }
+                }
+            } /* if on bottom */ else if (i == counts.size() - 1) {
+                for (int y = i - 1; y <= i; y++) {
+                    for (int x = j - 1; x <= j + 1; x++) {
+                        if (bombLocations[y][x]) bombs += 1;
+                    }
+                }
+            } /* if on left */ else if (j == 0) {
+                for (int y = i - 1; y <= i + 1; y++) {
+                    for (int x = j; x <= j + 1; x++) {
+                        if (bombLocations[y][x]) bombs += 1;
+                    }
+                }
+            } /* if on right */ else if (j == counts.size() - 1) {
+                for (int y = i - 1; y <= i + 1; y++) {
+                    for (int x = j - 1; x <= j; x++) {
+                        if (bombLocations[y][x]) bombs += 1;
                     }
                 }
             } /* if in middle */ else {
-                for (int x = i - 1; x <= i + 1; x++) {
-                    for (int y = j - 1; y <= j + 1; y++) {
-                        if (bombLocations[x][y]) bombs += 1;
+                for (int y = i - 1; y <= i + 1; y++) {
+                    for (int x = j - 1; x <= j + 1; x++) {
+                        if (bombLocations[y][x]) bombs += 1;
                     }
                 }
             }
@@ -55,9 +79,21 @@ vector<vector<int>> makeGridOfCounts(vector<vector<bool>> & bombLocations) {
 int main() {
     vector<bool> row(9, true);
     vector<vector<bool>> testMinesweeper(9, row);
-    vector<vector<int>> testCounts = makeGridOfCounts(testMinesweeper);
+    testMinesweeper[0][0] = false;
+    // printing Minesweeper grid
+    cout << "Minesweeper Grid: " << endl;
+    for (int i = 0; i < testMinesweeper.size(); i++) {
+        for (int j = 0; j < testMinesweeper[i].size(); j++) {
+            if (testMinesweeper[i][j]) cout << "True  ";
+            else cout << "False ";
+        }
+        cout << endl;
+    }
+    cout << endl;
     
     // printing grid of counts
+    vector<vector<int>> testCounts = makeGridOfCounts(testMinesweeper);
+    cout << "Grid of Counts: " << endl;
     for (int i = 0; i < testCounts.size(); i++) {
         for (int j = 0; j < testCounts[i].size(); j++) {
             cout << testCounts[i][j] << " ";
