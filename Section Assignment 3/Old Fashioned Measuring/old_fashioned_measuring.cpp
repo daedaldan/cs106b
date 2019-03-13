c267e5027ee00710766bc9b11876693e691afa03
Dan Wang <daniel22373@gmail.com>
2019-03-13 15:40:12 -0400

    Started 3.7

---
+++ Section Assignment 3/Old Fashioned Measuring/old_fashioned_measuring.cpp
@@ -0,0 +1,16 @@
+// CS106B: Programming Abstractions Assignment 3 Problem 7
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isMeasurable(int target, vector<int> & weights) {
+    
+}
+
+int main() {
+    vector<int>
+    
+    return 0;
+}
