c999903784f3513f5526c9a9f191c5ac22b84693
Dan Wang <daniel22373@gmail.com>
2019-03-14 18:08:40 -0400

    Reimplemented recursive backtracking code in 3.7

--- Section Assignment 3/Old Fashioned Measuring/old_fashioned_measuring.cpp
+++ Section Assignment 3/Old Fashioned Measuring/old_fashioned_measuring.cpp
@@ -6,28 +6,20 @@
 using namespace std;
 
 bool isMeasurableHelper(int target, vector<int> weights, int left, int right) {
+    cout << left << ", " << right << endl;
     if (left == right)
         return true;
     
     for (int i = 0; i < weights.size(); i++) {
-        for (int j = 0; j < 2; j++) {
-            vector<int>::iterator it = find(weights.begin(), weights.end(), weights[i]);
-            int currentNum = weights[i];
-            if (j == 0) {
-                continue;
-            } else if (j == 1) {
-                left += weights[i];
-            } else if (j == 2) {
-                right += weights[i];
-            }
-            weights.erase(it);
-            bool c = isMeasurableHelper(target, weights, left, right);
-            if (c)
-                return true;
-            else
-                weights.insert(it, currentNum);
+        int currentWeight = weights[i];
+        cout << "Current weight is: " << currentWeight << endl;
+        vector<int>::iterator it = find(weights.begin(), weights.end(), weights[i]);
+        weights.erase(it);
+        if (isMeasurableHelper(target, weights, left, right)) return true;
+        if (isMeasurableHelper(target, weights, left += currentWeight, right)) return true;
+        if (isMeasurableHelper(target, weights, left, right += currentWeight)) return true;
+        weights.insert(it, currentWeight);
         }
-    }
     
     return false;
 }
@@ -41,7 +33,7 @@ int main() {
     test1.push_back(1);
     test1.push_back(3);
     
-    if (isMeasurable(3, test1))
+    if (isMeasurable(2, test1))
         cout << "True" << endl;
     else
         cout << "False" << endl;
