37916eca52976fabf01e16098cc0413007858644
Daniel Wang <daniel22373@gmail.com>
2019-03-24 12:10:59 -0400

    Finished 4.4

--- Section Assignment 4/Linked List Warmup/linked_list_warmup.cpp
+++ Section Assignment 4/Linked List Warmup/linked_list_warmup.cpp
@@ -3,18 +3,34 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 struct Cell {
 	Cell *next;
 	int value;
 };
 
-Cell * convertToList(vector<int> v) {
-	Cell * head;
-	head->value = v[0];
+void printList(Cell * list) {
+	cout << list->value << endl;
+	if (list->next == NULL)
+		return;
+	else
+		printList(list->next);
+}
 
+int sumList(Cell * list) {
+	if (list->next == NULL)
+		return list->value;
+	else 
+		return list->value + sumList(list->next);
+}
+
+Cell * convertToList(vector<int> & v) {
+	Cell * head = new Cell;
+	head->value = v[0];
 	Cell * previous = head;
 	for (int i = 1; i < v.size(); i++) {
-		Cell * c;
+		Cell * c = new Cell;
 		c->value = v[i];
 		previous->next = c;
 		previous = c;
@@ -25,24 +41,22 @@ Cell * convertToList(vector<int> v) {
 	return head;
 }
 
-void printList(Cell * list) {
-	cout << list->value << endl;
-	if (list->next == NULL)
-		return
-	else
-		printList(list->next);
-}
+
 
 int main() {
-	vector<int> testVector;
+	vector<int> test1;
 	test1.push_back(0);
 	test1.push_back(1);
 	test1.push_back(2);
 	test1.push_back(3);
 
-	Cell * testLinkedList = convertToList(testVector);
+	Cell * testLinkedList = convertToList(test1);
 
+	cout << "Elements in testLinkedList:" << endl;
 	printList(testLinkedList);
+
+	cout << "Sum of the elements in testLinkedList:" << endl;
+	cout << sumList(testLinkedList) << endl;
 
 	return 0;
 }
