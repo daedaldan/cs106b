5882d03e06197353eb193d6cb60f49aff71aa16c
Daniel Wang <daniel22373@gmail.com>
2019-03-25 17:23:03 -0400

    Started 6.4

---
+++ Section Assignment 6/Filter Queue/filter_queue.cpp
@@ -0,0 +1,23 @@
+// CS106B: Programming Abstractions Assignment 6 Problem 4
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct movieT {
+	string title;
+	string genre;
+	int rating;
+};
+
+bool isBadComedy(movieT & m) {
+	return (m.genre == "comedy" && m.rating < 7);
+}
+
+void filter(queue<Type> & q, bool * )
+
+int main() {
+	
+	return 0;
+}
