1cf7467140c59dc8c11419a72e8bbd195f129399
Daniel Wang <daniel22373@gmail.com>
2019-03-27 16:43:43 -0400

    Finished 6.5

---
+++ Remove Duplicates/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Remove Duplicates
+//
+//  Created by Jianfeng Wang on 3/27/19.
+//  Copyright © 2019 Daniel Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
