9300e1494f47fbf0d2c1b4b15e4092486cc79b76
Daniel Wang <daniel22373@gmail.com>
2019-03-31 12:14:14 -0400

    Started 8.3

---
+++ Section Assignment 8/Tree Equal/tree_equal.cpp
@@ -0,0 +1,39 @@
+// CS106B: Programming Abstractions Assignment 8 Problem 3
+
+#include <iostream>
+
+using namespace std;
+
+struct nodeT {
+    string key;
+    nodeT * left;
+    nodeT * right;
+};
+
+void printBinaryTree(nodeT * root) {
+    
+}
+
+bool treeEqual(nodeT * tree1, nodeT * tree2) {
+    if (tree1->key != tree2->key) {
+        return false;
+    } else if (tree1->left == NULL && tree2->left != NULL) {
+        return false;
+    } else if (tree1->right == NULL && tree2->right != NULL) {
+        return false;
+    } else if (tree1->left != NULL && tree2->left == NULL) {
+        return false;
+    } else if (tree1->right != NULL && tree2->right == NULL) {
+        return false;
+    } else {
+        treeEqual(tree1->left, tree2->left);
+        treeEqual(tree1->right, tree2->right);
+    }
+    
+    return true;
+}
+
+int main() {
+    
+    return 0;
+}
