1d7dbf3b3e83569159a3639bb28148a67e8ce99a
Daniel Wang <daniel22373@gmail.com>
2019-03-31 14:08:39 -0400

    Finished 8.3

--- Section Assignment 8/Tree Equal/tree_equal.cpp
+++ Section Assignment 8/Tree Equal/tree_equal.cpp
@@ -1,4 +1,5 @@
 // CS106B: Programming Abstractions Assignment 8 Problem 3
+// code to print binary tree is from www.geeksforgeeks.org/print-binary-tree-2-dimensions/
 
 #include <iostream>
 
@@ -10,8 +11,136 @@ struct nodeT {
     nodeT * right;
 };
 
-void printBinaryTree(nodeT * tree) {
+// Function to print binary tree in 2D
+// It does reverse inorder traversal
+void print2DUtil(nodeT * root, int space) {
+    // Base case
+    if (root == NULL)
+        return;
     
+    // Increase distance between levels
+    space += 10;
+    
+    // Process right child first
+    print2DUtil(root->right, space);
+    
+    // Print current node after space
+    // count
+    cout<<endl;
+    for (int i = 10; i < space; i++)
+        cout<<" ";
+    cout<<root->key<<"\n";
+    
+    // Process left child
+    print2DUtil(root->left, space);
+}
+
+// Wrapper over print2DUtil()
+void print2D(nodeT * root)
+{
+    // Pass initial space count as 0
+    print2DUtil(root, 0);
+}
+
+nodeT * buildFruitTree1() {
+    nodeT * root = new nodeT;
+    root->key = "Orange";
+    
+    nodeT * left = new nodeT;
+    left->key = "Pear";
+    nodeT * right = new nodeT;
+    right->key = "Watermelon";
+    
+    nodeT * left1 = new nodeT;
+    left1->key = "Apple";
+    nodeT * right1 = new nodeT;
+    right1->key = "Coconut";
+    
+    nodeT * left2 = new nodeT;
+    left2->key = "Strawberry";
+    nodeT * right2 = new nodeT;
+    right2->key = "Pineapple";
+    
+    root->left = left;
+    root->right = right;
+    
+    left->left = left1;
+    left->right = right1;
+    
+    right->left = left2;
+    right->right = right2;
+    
+    left1->left = NULL;
+    right1->left = NULL;
+    left1->right = NULL;
+    right1->right = NULL;
+    left2->left = NULL;
+    right2->left = NULL;
+    left2->right = NULL;
+    right2->right = NULL;
+    
+    return root;
+}
+
+nodeT * buildFruitTree2() {
+    nodeT * root = new nodeT;
+    root->key = "Lychee";
+    
+    nodeT * left = new nodeT;
+    left->key = "Banana";
+    nodeT * right = new nodeT;
+    right->key = "Watermelon";
+    
+    nodeT * left1 = new nodeT;
+    left1->key = "Apple";
+    nodeT * right1 = new nodeT;
+    right1->key = "Coconut";
+    
+    nodeT * left2 = new nodeT;
+    left2->key = "Kiwi";
+    nodeT * right2 = new nodeT;
+    right2->key = "Pineapple";
+    
+    root->left = left;
+    root->right = right;
+    
+    left->left = left1;
+    left->right = right1;
+    
+    right->left = left2;
+    right->right = right2;
+    
+    left1->left = NULL;
+    right1->left = NULL;
+    left1->right = NULL;
+    right1->right = NULL;
+    left2->left = NULL;
+    right2->left = NULL;
+    left2->right = NULL;
+    right2->right = NULL;
+    
+    return root;
+}
+
+nodeT * buildFruitTree3() {
+    nodeT * root = new nodeT;
+    root->key = "Pear";
+    
+    nodeT * left = new nodeT;
+    left->key = "Dragonfruit";
+    nodeT * right = new nodeT;
+    right->key = "Apple";
+    
+    
+    root->left = left;
+    root->right = right;
+    
+    left->left = NULL;
+    right->left = NULL;
+    left->right = NULL;
+    right->right = NULL;
+
+    return root;
 }
 
 bool treeEqual(nodeT * tree1, nodeT * tree2) {
@@ -25,6 +154,8 @@ bool treeEqual(nodeT * tree1, nodeT * tree2) {
         return false;
     } else if (tree1->right != NULL && tree2->right == NULL) {
         return false;
+    } else if ((tree1->left == NULL && tree2->left == NULL) || (tree1->right == NULL && tree2->right == NULL)) {
+        // do nothing
     } else {
         treeEqual(tree1->left, tree2->left);
         treeEqual(tree1->right, tree2->right);
@@ -34,6 +165,25 @@ bool treeEqual(nodeT * tree1, nodeT * tree2) {
 }
 
 int main() {
+    nodeT * test1 = buildFruitTree1();
+    nodeT * test2 = buildFruitTree2();
+    nodeT * test3 = buildFruitTree3();
+    nodeT * test4 = buildFruitTree1();
+    
+    if (treeEqual(test1, test4))
+        cout << "Fruit tree 1 is equal to fruit tree 4" << endl;
+    else
+        cout << "Fruit tree 1 is not equal to fruit tree 4" << endl;
+    
+    if (treeEqual(test1, test2))
+        cout << "Fruit tree 1 is equal to fruit tree 2" << endl;
+    else
+        cout << "Fruit tree 1 is not equal to fruit tree 2" << endl;
+    
+    if (treeEqual(test2, test3))
+        cout << "Fruit tree 2 is equal to fruit tree 3" << endl;
+    else
+        cout << "Fruit tree 2 is not equal to fruit tree 3" << endl;
     
     return 0;
 }
