a25c522fec39f1a95f95ab21ea60cd8673e43030
Daniel Wang <daniel22373@gmail.com>
2019-03-31 11:58:20 -0400

    Stuck on 7.4, moving on

--- Section Assignment 7/Unstutter/unstutter.cpp
+++ Section Assignment 7/Unstutter/unstutter.cpp
@@ -34,47 +34,88 @@ Cell * convertToList(vector<int> & v) {
     return head;
 }
 
+// helper function for unstutter
+void removeLastCell(Cell * list) {
+    if (list->next->next == NULL) {
+        list->next = NULL;
+    } else {
+        list->value = list->next->value;
+        list = list->next;
+        removeLastCell(list);
+    }
+}
+
 void unstutter(Cell * list) {
     Cell * current = new Cell;
-    current->value = list->value;
     current->next = list->next;
+    current->value = list->value;
+
+    list->next = current;
     
     while (current->next) {
         if (current->next->value == current->value) {
-            Cell * newNext = current->next->next;
-            current->next = newNext;
+            current->next = current->next->next;
         } else {
             current->value = current->next->value;
             current = current->next;
         }
     }
+    
+    // manually removing duplicate cell at end (not sure why it appears)
+    Cell * copy = new Cell;
+    Cell * previous = new Cell;
+    copy = list;
+    
+    while (copy->next) {
+        previous = copy;
+        copy->value = copy->next->value;
+        copy = copy->next;
+    }
+    
+    previous->next = NULL;
 }
 
 int main() {
-    vector<int> test;
-    test.push_back(11);
-    test.push_back(11);
-    test.push_back(5);
-    test.push_back(5);
-    test.push_back(3);
-    test.push_back(21);
-    test.push_back(21);
-    test.push_back(7);
-    test.push_back(7);
-    test.push_back(7);
-    test.push_back(4);
-    test.push_back(7);
+    vector<int> test1;
+    test1.push_back(11);
+    test1.push_back(11);
+    test1.push_back(5);
+    test1.push_back(5);
+    test1.push_back(3);
+    test1.push_back(21);
+    test1.push_back(21);
+    test1.push_back(7);
+    test1.push_back(7);
+    test1.push_back(7);
+    test1.push_back(4);
+    test1.push_back(7);
     
-    Cell * testList = convertToList(test);
+    Cell * testList1 = convertToList(test1);
     
     cout << "List before unstutter:" << endl;
-    printList(testList);
+    printList(testList1);
     
     cout << endl;
-    unstutter(testList);
+    unstutter(testList1);
     
     cout << "List after unstutter:" << endl;
-    printList(testList);
+    printList(testList1);
+    cout << endl;
+    
+    
+    vector<int> test2;
+    test2.push_back(0);
+    
+    Cell * testList2 = convertToList(test2);
+    
+    cout << "List before unstutter:" << endl;
+    printList(testList2);
+    
+    cout << endl;
+    unstutter(testList2);
+    
+    cout << "List after unstutter:" << endl;
+    printList(testList2);
     
     return 0;
 }
