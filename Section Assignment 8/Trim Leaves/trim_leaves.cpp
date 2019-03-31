0945ed4d6ab5650a689a19beb74284b17ea93420
Daniel Wang <daniel22373@gmail.com>
2019-03-31 12:17:52 -0400

    Started 8.4

---
+++ Section Assignment 8/Trim Leaves/trim_leaves.cpp
@@ -0,0 +1,24 @@
+// CS106B: Programming Abstractions Assignment 8 Problem 4
+
+#include <iostream>
+
+using namespace std;
+
+struct nodeT {
+    string key;
+    nodeT * left;
+    nodeT * right;
+};
+
+void printBinaryTree(nodeT * root) {
+    
+}
+
+void trimLeaves(nodeT * & tree1) {
+
+}
+
+int main() {
+    
+    return 0;
+}
